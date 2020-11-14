c8e463cbf1bb483db5264f05467210edcbaded8a
Joseph Burke <joseph_burke@outlook.com>
2020-11-14 16:41:23 +0000

    Add content and styles to contact page.

--- src/content/contact.js
+++ src/content/contact.js
@@ -4,7 +4,42 @@ export default function generateContact() {
   contactSection.classList.add("landing-page-section", "contact-tab", "container-fluid");
   contactSection.id = 'landing-page-section';
 
-  contactSection.innerText = 'Hello, this is the contact section';
+  let contactCard = document.createElement('div');
+  contactCard.classList.add('card', 'card-body');
+  
+  let title =  document.createElement('h5');
+  title.classList.add('card-title', 'text-center');
+  title.innerText = 'Como Contactarnos';
+  
+  let text = document.createElement('p');
+  text.classList.add('card-text');
+  text.innerText = 'Si tiene preguntas o le gustaría hacer una reservación, somos disponibles por teléfono, Internet y en persona durante nuestras horas de trabajo.';
+  
+  let lineBreak1 = document.createElement('br');
+  let lineBreak2 = document.createElement('br');
+  
+  let telephone= document.createElement('p');
+  telephone.innerText = 'Tel: +43 18402 290 117';
+  
+  let email = document.createElement('p');
+  email.innerText = 'E-mail: amigos@restaurante.com';
+  
+  let address = document.createElement('p');
+  address.innerText = 'Dirección: 472 Calle de la República, Cuenca, Ecuador';
+  
+  let map = document.createElement('img');
+  map.src = '../src/map.jpeg';
+  
+  text.appendChild(lineBreak1);
+  text.appendChild(lineBreak2);
+  text.appendChild(telephone);
+  text.appendChild(email);
+  text.appendChild(address);
+  text.appendChild(map);
+
+  contactCard.appendChild(title);
+  contactCard.appendChild(text);
+  contactSection.appendChild(contactCard);
 
   return contactSection;
 }
--- src/content/menu.js
+++ src/content/menu.js
@@ -2,17 +2,17 @@ const dishes = [
   {
     name: "Dahl Indiano Auténtico",
     description:
-      "Ese plato sencillo y delicisio tiene su origenes en India del norte y Nepal. Hecho de lentejas y cebollas y lleno de proteína, nuestro dahl es perfecto para ellos que desean a construír musclos - o justo disfrutar sus lindos sabores.",
+      "Ese plato sencillo y delicisio tiene sus origenes en India del norte y Nepal. Hecho de lentejas y cebollas y lleno de proteína, nuestro dahl es perfecto para ellos que desean a construír musclos - o justo disfrutar sus lindos sabores.",
     "image URL": "../src/dahl.jpg"
   },
   {
     name: "Ensalada Temporada y Local",
-    description: "Aquí a Amigos, creemos fuerzamente en nuestra obligación de hacer bueno durante nustro tiempo sobre el planeta. Por eso, estámos todos muy orgullos a offrir esta ensalada temporada y local que hace bueno para la salud de nuestros clientes, nuestra comunidad local, y el planeta.",
+    description: "Aquí a Amigos, creemos fuerzamente en nuestra obligación de hacer bueno durante nuestro tiempo sobre el planeta. Por eso, estámos todos muy orgullos a offrir esta ensalada temporada y local que hace bueno para la salud de nuestros clientes, nuestra comunidad local, y el planeta.",
     "image URL": "../src/salad.jpg"
   },
   {
     name: "La Pizza de la Amistad",
-    description: "Nuestro tercer plato - y probablamente nuestro favorito - es \"la Pizza de Amistad\". Desiñada para que la coman cuatro amigos, esta pizza mide cincuenta centimetros de ancho y puede ordenarse con cualquieres ingredientes usted y sus amigos quieran. Como si todo eso no fuera suficiente, Amigos ofrecemos una descuenta de 15% en todas las cervezas para las mesas que ordenan la Pizza de Amistad. Disfruta!",
+    description: "Nuestro tercer plato - y probablamente nuestro favorito - es \"la Pizza de la Amistad\". Desiñada para que la coman cuatro amigos, esta pizza mide cincuenta centimetros de ancho y puede ordenarse con cualquieres ingredientes usted y sus amigos quieran. Como si todo eso no fuera suficiente, Amigos ofrecemos una descuenta de 15% en todas las cervezas para las mesas que ordenan la Pizza de la Amistad. Disfruta!",
     "image URL": "../src/pizza.jpg"
   }
 ];
